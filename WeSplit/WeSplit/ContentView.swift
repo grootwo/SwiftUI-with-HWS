877eb1e190a68d3cf93ec3072bd2856f94520723
soaringwave <82134672+soaringwave@users.noreply.github.com>
2024-04-26 14:31:04 +0900

    Set navigation title

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -9,15 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Form {
-            Section {
-                Text("This is a Form in Section 1")
-                Text("This is a Form")
+        NavigationStack {
+            Form {
+                Section {
+                    Text("This is a Form in Section 1")
+                    Text("This is a Form")
+                }
+                Section {
+                    Text("This is a Form in Section 2")
+                    Text("This is a Form")
+                }
+                Section {
+                    Text("This is a Form in Section 3")
+                    Text("This is a Form")
+                }
+                Section {
+                    Text("This is a Form in Section 4")
+                    Text("This is a Form")
+                }
+                Section {
+                    Text("This is a Form in Section 5")
+                    Text("This is a Form")
+                }
             }
-            Section {
-                Text("This is a Form in Section 2")
-                Text("This is a Form")
-            }
+            .navigationTitle("Navigation title")
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
