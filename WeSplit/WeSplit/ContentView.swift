f9ea93be2d2d7d83d4b4c04176a06828b205d2c1
soaringwave <82134672+soaringwave@users.noreply.github.com>
2024-04-26 16:00:26 +0900

    Create Picker with ForEach

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -10,11 +10,18 @@ import SwiftUI
 struct ContentView: View {
     @State var tapCount = 0
     @State private var text = ""
+    @State private var selected = "orange"
+    let picks = ["orange", "watermelon", "lemon", "apple"]
     var body: some View {
         NavigationStack {
             Form {
                 TextField("Enter text", text: $text)
                 Text("Entered text is \(text)")
+                Picker("Select your favorite", selection: $selected) {
+                    ForEach(picks, id: \.self) { pick in
+                        Text(pick)
+                    }
+                }
                 Section {
                     Text("This is a Form in Section 1")
                     Text("This is a Form")
