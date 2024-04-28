2a29f5a74abcb6c7bc131a0461aa25803c58b0e0
soaringwave <82134672+soaringwave@users.noreply.github.com>
2024-04-28 11:58:57 +0900

    Make tip percentage various with navigationlink

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -40,11 +40,11 @@ struct ContentView: View {
                 }
                 Section("How much do you want to tip?") {
                     Picker("Tip percentage", selection: $tipPercentage) {
-                        ForEach(tipPercentages, id: \.self) {
+                        ForEach(0..<101) {
                             Text($0, format: .percent)
                         }
                     }
-                    .pickerStyle(.segmented)
+                    .pickerStyle(.navigationLink)
                 }
                 Section("Total amount") {
                     Text(totalAmount, format: .currency(code: "USD"))
