dd532d5a411a579cc698104636904498311836d4
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-06-27 15:19:56 +0900

    Feat: create Bookworm project

---
+++ Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Groo on 6/27/24.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
