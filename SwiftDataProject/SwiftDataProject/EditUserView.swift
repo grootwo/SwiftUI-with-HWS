12793648352e0002250f17536766b6f4d9128b2c
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-07-03 14:08:26 +0900

    Feat: create User and set EditUserView

---
+++ SwiftDataProject/SwiftDataProject/EditUserView.swift
@@ -0,0 +1,34 @@
+//
+//  EditUserView.swift
+//  SwiftDataProject
+//
+//  Created by Groo on 7/3/24.
+//
+
+import SwiftUI
+import SwiftData
+
+struct EditUserView: View {
+    @Bindable var user: User
+    var body: some View {
+        Form {
+            TextField("Name", text: $user.name)
+            TextField("City", text: $user.city)
+            DatePicker("Join Date", selection: $user.joinDate, displayedComponents: .date)
+        }
+        .navigationTitle("Edit User")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    do {
+        let config = ModelConfiguration(isStoredInMemoryOnly: true)
+        let container = try ModelContainer(for: User.self, configurations: config)
+        let user = User(name: "Unknown", city: "Somewhere", joinDate: .now)
+        return EditUserView(user: user)
+            .modelContainer(container)
+    } catch {
+        return Text("Failed to create container: \(error.localizedDescription)")
+    }
+}
---
+++ SwiftDataProject/SwiftDataProject/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  SwiftDataProject
+//
+//  Created by Groo on 7/3/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class User {
+    var name: String
+    var city: String
+    var joinDate: Date
+    
+    init(name: String, city: String, joinDate: Date) {
+        self.name = name
+        self.city = city
+        self.joinDate = joinDate
+    }
+}
