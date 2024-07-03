3b97bbeada7ab4c44e3c85a94fe58b810580d03c
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-07-03 14:56:27 +0900

    Feat: add filter to query

--- SwiftDataProject/SwiftDataProject/ContentView.swift
+++ SwiftDataProject/SwiftDataProject/ContentView.swift
@@ -10,7 +10,10 @@ import SwiftData
 
 struct ContentView: View {
     @Environment(\.modelContext) var modelContext
-    @Query(sort: \User.name) var users: [User]
+    @Query(filter: #Predicate<User> { user in
+        user.name.localizedStandardContains("r") &&
+        user.city == "New York"
+    }, sort: \User.name) var users: [User]
     var body: some View {
         NavigationStack {
             List(users) { user in
@@ -21,7 +24,16 @@ struct ContentView: View {
             .navigationTitle("Users")
             .toolbar {
                 Button("Add user", systemImage: "plus") {
-                   
+                    try? modelContext.delete(model: User.self)
+                    let first = User(name: "Ed Sheeran", city: "London", joinDate: .now.addingTimeInterval(86400 * -10))
+                    let second = User(name: "Rosa Diaz", city: "New York", joinDate: .now.addingTimeInterval(86400 * -5))
+                    let third = User(name: "Roy Kent", city: "London", joinDate: .now.addingTimeInterval(86400 * 5))
+                    let fourth = User(name: "Johnny English", city: "London", joinDate: .now.addingTimeInterval(86400 * 10))
+                    
+                    modelContext.insert(first)
+                    modelContext.insert(second)
+                    modelContext.insert(third)
+                    modelContext.insert(fourth)
                 }
             }
         }
