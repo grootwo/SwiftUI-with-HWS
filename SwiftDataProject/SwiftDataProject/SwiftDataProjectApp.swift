abca9e32d6dd8e638e4734905154dca50609a812
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-07-03 13:52:04 +0900

    Feat: create SwiftDataProject

---
+++ SwiftDataProject/SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Groo on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
