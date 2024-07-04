5a128ca02cda3eafc00f43e5d4567e4b533d5646
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-07-04 14:15:50 +0900

    Feat: create UsersView

---
+++ SwiftDataProject/SwiftDataProject/UsersView.swift
@@ -0,0 +1,32 @@
+//
+//  UsersView.swift
+//  SwiftDataProject
+//
+//  Created by Groo on 7/4/24.
+//
+
+import SwiftUI
+import SwiftData
+
+struct UsersView: View {
+    @Query var users: [User]
+    
+    var body: some View {
+        List(users) { user in
+            NavigationLink(value: user) {
+                Text(user.name)
+            }
+        }
+    }
+    
+    init(minimumJoinDate: Date) {
+        _users = Query(filter: #Predicate<User> { user in
+            user.joinDate >= minimumJoinDate
+        }, sort: \User.name)
+    }
+}
+
+#Preview {
+    UsersView()
+        .modelContainer(for: User.self)
+}
