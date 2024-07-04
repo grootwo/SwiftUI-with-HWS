7b4b7b483685ddbfcf4f002c16819a6701437696
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-07-04 14:39:44 +0900

    Feat: set dynamic sort order with SortDescriptor

--- SwiftDataProject/SwiftDataProject/ContentView.swift
+++ SwiftDataProject/SwiftDataProject/ContentView.swift
@@ -11,27 +11,42 @@ import SwiftData
 struct ContentView: View {
     @Environment(\.modelContext) var modelContext
     @State private var isShowingUpcoming = false
+    @State private var sortOrder = [SortDescriptor(\User.name), SortDescriptor(\User.joinDate)]
     var body: some View {
         NavigationStack {
-            UsersView(minimumJoinDate: isShowingUpcoming ? .now : .distantPast)
-            .navigationTitle("Users")
-            .toolbar {
-                Button("Add user", systemImage: "plus") {
-                    try? modelContext.delete(model: User.self)
-                    let first = User(name: "Ed Sheeran", city: "London", joinDate: .now.addingTimeInterval(86400 * -10))
-                    let second = User(name: "Rosa Diaz", city: "New York", joinDate: .now.addingTimeInterval(86400 * -5))
-                    let third = User(name: "Roy Kent", city: "London", joinDate: .now.addingTimeInterval(86400 * 5))
-                    let fourth = User(name: "Johnny English", city: "London", joinDate: .now.addingTimeInterval(86400 * 10))
-                    
-                    modelContext.insert(first)
-                    modelContext.insert(second)
-                    modelContext.insert(third)
-                    modelContext.insert(fourth)
+            UsersView(minimumJoinDate: isShowingUpcoming ? .now : .distantPast, sortOrder: sortOrder)
+                .navigationTitle("Users")
+                .toolbar {
+                    Button("Add user", systemImage: "plus") {
+                        try? modelContext.delete(model: User.self)
+                        let first = User(name: "Ed Sheeran", city: "London", joinDate: .now.addingTimeInterval(86400 * -10))
+                        let second = User(name: "Rosa Diaz", city: "New York", joinDate: .now.addingTimeInterval(86400 * -5))
+                        let third = User(name: "Roy Kent", city: "London", joinDate: .now.addingTimeInterval(86400 * 5))
+                        let fourth = User(name: "Johnny English", city: "London", joinDate: .now.addingTimeInterval(86400 * 10))
+                        
+                        modelContext.insert(first)
+                        modelContext.insert(second)
+                        modelContext.insert(third)
+                        modelContext.insert(fourth)
+                    }
+                    Button(isShowingUpcoming ? "Show everyone" : "Show upcoming") {
+                        isShowingUpcoming.toggle()
+                    }
+                    Menu("Sort", systemImage: "arrow.up.arrow.down") {
+                        Picker("Sort", selection: $sortOrder) {
+                            Text("Sort by name")
+                                .tag([
+                                    SortDescriptor(\User.name),
+                                    SortDescriptor(\User.joinDate)
+                                ])
+                            Text("Sort by name")
+                                .tag([
+                                    SortDescriptor(\User.joinDate),
+                                    SortDescriptor(\User.name)
+                                ])
+                        }
+                    }
                 }
-                Button(isShowingUpcoming ? "Show everyone" : "Show upcoming") {
-                    isShowingUpcoming.toggle()
-                }
-            }
         }
     }
 }
