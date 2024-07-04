22ed29473a1cfdb4fd9329427a8fb8816f9d9524
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-07-04 15:22:43 +0900

    Feat: Create Job and relate with User

--- SwiftDataProject/SwiftDataProject/UsersView.swift
+++ SwiftDataProject/SwiftDataProject/UsersView.swift
@@ -10,11 +10,20 @@ import SwiftData
 
 struct UsersView: View {
     @Query var users: [User]
+    @Environment(\.modelContext) var modelContext
     
     var body: some View {
         List(users) { user in
             NavigationLink(value: user) {
-                Text(user.name)
+                HStack {
+                    Text(user.name)
+                    Spacer()
+                    Text(String(user.jobs.count))
+                        .padding(7)
+                        .foregroundStyle(.white)
+                        .background(.secondary)
+                        .clipShape(.circle)
+                }
             }
         }
     }
@@ -23,6 +32,15 @@ struct UsersView: View {
         _users = Query(filter: #Predicate<User> { user in
             user.joinDate >= minimumJoinDate
         }, sort: sortOrder)
+    }
+    
+    func addSample() {
+        let user1 = User(name: "unknown", city: "somewhere", joinDate: .now)
+        var job1 = Job(name: "Meeting with workers", priority: 2)
+        var job2 = Job(name: "Eating lunch", priority: 1)
+        modelContext.insert(user1)
+        user1.jobs.append(job1)
+        user1.jobs.append(job2)
     }
 }
 
