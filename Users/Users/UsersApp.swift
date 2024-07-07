1299dd6c71753096414bb5e277b340e4363af69c
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-07-07 13:41:35 +0900

    Feat: create Users project

---
+++ Users/Users/UsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsersApp.swift
+//  Users
+//
+//  Created by Groo on 7/7/24.
+//
+
+import SwiftUI
+
+@main
+struct UsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
