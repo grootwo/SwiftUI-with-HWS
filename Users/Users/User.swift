c2d365996012032800733af69913e534d5a8f631
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-07-07 14:13:34 +0900

    Feat: create User and Friend

---
+++ Users/Users/User.swift
@@ -0,0 +1,26 @@
+//
+//  User.swift
+//  Users
+//
+//  Created by Groo on 7/7/24.
+//
+
+import Foundation
+
+struct User: Codable {
+    var id: String
+    var isActive: Bool
+    var name: String
+    var age: Int
+    var company: String
+    var email: String
+    var address: String
+    var registered: Date
+    var tage: [String]
+    var friends: [Friend]
+}
+
+struct Friend: Codable {
+    var id: String
+    var name: String
+}
