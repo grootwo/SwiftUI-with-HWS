f75dbeb5b71c3521dcb03550afcfe52421a1f143
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-07-08 14:26:37 +0900

    Feat: set User and Friend as Model with decoding initializer and encoding func

--- Users/Users/User.swift
+++ Users/Users/User.swift
@@ -6,8 +6,24 @@
 //
 
 import Foundation
+import SwiftData
 
-struct User: Codable {
+@Model
+class User: Codable {
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case isActive = "isActive"
+        case name = "name"
+        case age = "age"
+        case company = "company"
+        case email = "email"
+        case address = "address"
+        case about = "about"
+        case registered = "registered"
+        case tags = "tags"
+        case friends = "friends"
+    }
+    
     var id: String
     var isActive: Bool
     var name: String
@@ -19,9 +35,57 @@ struct User: Codable {
     var registered: String
     var tags: [String]
     var friends: [Friend]
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decode(String.self, forKey: .id)
+        self.isActive = try container.decode(Bool.self, forKey: .isActive)
+        self.name = try container.decode(String.self, forKey: .name)
+        self.age = try container.decode(Int.self, forKey: .age)
+        self.company = try container.decode(String.self, forKey: .company)
+        self.email = try container.decode(String.self, forKey: .email)
+        self.address = try container.decode(String.self, forKey: .address)
+        self.about = try container.decode(String.self, forKey: .about)
+        self.registered = try container.decode(String.self, forKey: .registered)
+        self.tags = try container.decode([String].self, forKey: .tags)
+        self.friends = try container.decode([Friend].self, forKey: .friends)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(self.id, forKey: .id)
+        try container.encode(self.isActive, forKey: .isActive)
+        try container.encode(self.name, forKey: .name)
+        try container.encode(self.age, forKey: .age)
+        try container.encode(self.company, forKey: .company)
+        try container.encode(self.email, forKey: .email)
+        try container.encode(self.address, forKey: .address)
+        try container.encode(self.about, forKey: .about)
+        try container.encode(self.registered, forKey: .registered)
+        try container.encode(self.tags, forKey: .tags)
+        try container.encode(self.friends, forKey: .friends)
+    }
 }
 
-struct Friend: Codable, Hashable {
+@Model
+class Friend: Codable, Hashable {
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case name = "name"
+    }
+    
     var id: String
     var name: String
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decode(String.self, forKey: .id)
+        self.name = try container.decode(String.self, forKey: .name)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(self.id, forKey: .id)
+        try container.encode(self.name, forKey: .name)
+    }
 }
