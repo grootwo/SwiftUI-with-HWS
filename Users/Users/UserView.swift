b043771e262599e51c3ad52e4b998c085152ede0
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-07-08 14:34:16 +0900

    Refactor: make preview comments

--- Users/Users/UserView.swift
+++ Users/Users/UserView.swift
@@ -52,14 +52,14 @@ struct UserView: View {
     }
 }
 
-#Preview {
-    UserView(user: User(id: "50a48fa3-2c0f-4397-ac50-64da464f9954", isActive: true, name: "Alford Rodriguez", age: 21, company: "Imkan", email: "alfordrodriguez@imkan.com", address: "907 Nelson Street, Cotopaxi, South Dakota, 5913", about: "Occaecat consequat elit aliquip magna laboris dolore laboris sunt officia adipisicing reprehenderit sunt. Do in proident consectetur labore. Laboris pariatur quis incididunt nostrud labore ad cillum veniam ipsum ullamco. Dolore laborum commodo veniam nisi. Eu ullamco cillum ex nostrud fugiat eu consequat enim cupidatat. Non incididunt fugiat cupidatat reprehenderit nostrud eiusmod eu sit minim do amet qui cupidatat. Elit aliquip nisi ea veniam proident dolore exercitation irure est deserunt.", registered: "2015-11-10T01:47:18-00:00", tags: [
-        "cillum",
-        "consequat",
-        "deserunt",
-        "nostrud",
-        "eiusmod",
-        "minim",
-        "tempor"
-    ], friends: [Friend(id: "91b5be3d-9a19-4ac2-b2ce-89cc41884ed0", name: "Hawkins Patel"), Friend(id: "5890bacd-f49c-4ea2-b8fa-02db0e083238", name: "Karin Collins")]))
-}
+//#Preview {
+//    UserView(user: User(id: "50a48fa3-2c0f-4397-ac50-64da464f9954", isActive: true, name: "Alford Rodriguez", age: 21, company: "Imkan", email: "alfordrodriguez@imkan.com", address: "907 Nelson Street, Cotopaxi, South Dakota, 5913", about: "Occaecat consequat elit aliquip magna laboris dolore laboris sunt officia adipisicing reprehenderit sunt. Do in proident consectetur labore. Laboris pariatur quis incididunt nostrud labore ad cillum veniam ipsum ullamco. Dolore laborum commodo veniam nisi. Eu ullamco cillum ex nostrud fugiat eu consequat enim cupidatat. Non incididunt fugiat cupidatat reprehenderit nostrud eiusmod eu sit minim do amet qui cupidatat. Elit aliquip nisi ea veniam proident dolore exercitation irure est deserunt.", registered: "2015-11-10T01:47:18-00:00", tags: [
+//        "cillum",
+//        "consequat",
+//        "deserunt",
+//        "nostrud",
+//        "eiusmod",
+//        "minim",
+//        "tempor"
+//    ], friends: [Friend(id: "91b5be3d-9a19-4ac2-b2ce-89cc41884ed0", name: "Hawkins Patel"), Friend(id: "5890bacd-f49c-4ea2-b8fa-02db0e083238", name: "Karin Collins")]))
+//}
