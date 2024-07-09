c6ae5abffaa42c0f01dac13df6a2c419d4c299ca
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-07-09 15:26:30 +0900

    Feat: set Text with blur modifier

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -8,12 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var blurAmout = 0.0 {
+        didSet {
+            print("blur amount: \(blurAmout)")
+        }
+    }
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("This is a text")
+                .blur(radius: blurAmout)
+            Slider(value: $blurAmout, in: 0...10)
+            Button("Blur text randomly") {
+                blurAmout = Double.random(in: 0...10)
+            }
         }
         .padding()
     }
