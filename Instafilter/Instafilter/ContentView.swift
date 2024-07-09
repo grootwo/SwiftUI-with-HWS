98dfb9bb87461bbd3f914696783c9fb847cf04b7
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-07-09 15:44:33 +0900

    Feat: set onChange for blurAmout

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -8,16 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var blurAmout = 0.0 {
-        didSet {
-            print("blur amount: \(blurAmout)")
-        }
-    }
+    @State private var blurAmout = 0.0
     var body: some View {
         VStack {
             Text("This is a text")
                 .blur(radius: blurAmout)
             Slider(value: $blurAmout, in: 0...10)
+                .onChange(of: blurAmout) { oldValue, newValue in
+                    print("blur amount: \(newValue)")
+                }
             Button("Blur text randomly") {
                 blurAmout = Double.random(in: 0...10)
             }
