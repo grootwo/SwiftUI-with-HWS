7b82ce56b694e50e371745873729266304cb3970
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-07-09 15:12:43 +0900

    Feat: create Instafilter project

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Groo on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
