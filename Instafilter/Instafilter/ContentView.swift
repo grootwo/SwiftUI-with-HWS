9f54421e0beb15712aec123806f75112c409141d
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-07-10 16:24:47 +0900

    Feat: manipulate image

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -6,6 +6,8 @@
 //
 
 import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
 
 struct ContentView: View {
     @State private var image: Image?
@@ -18,7 +20,26 @@ struct ContentView: View {
         .onAppear(perform: loadImage)
     }
     func loadImage() {
-        image = Image(.cat)
+        let inputImage = UIImage(resource: .cat)
+        let beginImage = CIImage(image: inputImage)
+        
+        let context = CIContext()
+        let currentFilter = CIFilter.sepiaTone()
+        
+        currentFilter.inputImage = beginImage
+        currentFilter.intensity = 1
+        
+        // get a CIImage from our filter or exit if that fails
+        guard let outputImage = currentFilter.outputImage else { return }
+
+        // attempt to get a CGImage from our CIImage
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
+
+        // convert that to a UIImage
+        let uiImage = UIImage(cgImage: cgImage)
+
+        // and convert that to a SwiftUI image
+        image = Image(uiImage: uiImage)
     }
 }
 
