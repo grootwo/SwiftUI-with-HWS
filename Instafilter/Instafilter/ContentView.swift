3ea51f5bc0999e15b78c1225332ca0cb9849400c
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-07-11 14:42:28 +0900

    Feat: set photosPicker to get several photos

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -9,18 +9,25 @@ import SwiftUI
 import PhotosUI
 
 struct ContentView: View {
-    @State private var pickerItem: PhotosPickerItem?
-    @State private var selectedImage: Image?
+    @State private var pickerItems = [PhotosPickerItem]()
+    @State private var selectedImages = [Image]()
     var body: some View {
         VStack {
-            PhotosPicker("Select a photo", selection: $pickerItem, matching: .images)
-            selectedImage?
-                .resizable()
-                .scaledToFit()
-        }
-        .onChange(of: pickerItem) {
+            PhotosPicker("Select a photo", selection: $pickerItems, matching: .images)
+            ScrollView {
+                ForEach(0..<selectedImages.count, id: \.self) { index in
+                    selectedImages[index]
+                        .resizable()
+                        .scaledToFit()
+                }
+            }        }
+        .onChange(of: pickerItems) {
             Task {
-                selectedImage = try await pickerItem?.loadTransferable(type: Image.self)
+                for item in pickerItems {
+                    if let loadedImage = try await item.loadTransferable(type: Image.self) {
+                        selectedImages.append(loadedImage)
+                    }
+                }
             }
         }
     }
