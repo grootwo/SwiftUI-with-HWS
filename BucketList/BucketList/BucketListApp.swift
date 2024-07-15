48eadc7df637c479cfc61947486a86fb532ac27e
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-07-16 08:57:26 +0900

    Feat: create BucketList project

---
+++ BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Groo on 7/16/24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
