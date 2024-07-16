451048892963b959c3c0506ecf34cb04e9f5cdd9
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-07-16 09:18:22 +0900

    Feat: create Person and sort with sorted

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -7,15 +7,24 @@
 
 import SwiftUI
 
+struct Person: Identifiable {
+    let id = UUID()
+    var firstName: String
+    var lastName: String
+}
+
 struct ContentView: View {
+    let people = [
+        Person(firstName: "철수", lastName: "팽"),
+        Person(firstName: "지현", lastName: "선우"),
+        Person(firstName: "민수", lastName: "제갈")
+    ].sorted {
+        $0.lastName < $1.lastName
+    }
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(people) { person in
+            Text("\(person.lastName) \(person.firstName)")
         }
-        .padding()
     }
 }
 
