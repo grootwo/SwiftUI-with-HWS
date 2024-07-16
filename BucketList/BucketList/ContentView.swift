801348da7f853a1a4f37e9381c3e4cc246dda8de
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-07-16 14:26:39 +0900

    Feat: write and read data

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -7,25 +7,18 @@
 
 import SwiftUI
 
-struct Person: Comparable, Identifiable {
-    let id = UUID()
-    var firstName: String
-    var lastName: String
-    
-    static func <(lhs: Person, rhs: Person) -> Bool {
-        lhs.lastName < rhs.lastName
-    }
-}
-
 struct ContentView: View {
-    let people = [
-        Person(firstName: "철수", lastName: "팽"),
-        Person(firstName: "지현", lastName: "선우"),
-        Person(firstName: "민수", lastName: "제갈")
-    ].sorted()
     var body: some View {
-        List(people) { person in
-            Text("\(person.lastName) \(person.firstName)")
+        Button("Write and Read the data") {
+            let data = Data("Test Data".utf8)
+            let url = URL.documentsDirectory.appending(path: "message.txt")
+            do {
+                try data.write(to: url, options: [.atomic, .completeFileProtection])
+                let input = try String(contentsOf: url)
+                print(input)
+            } catch {
+                print(error.localizedDescription)
+            }
         }
     }
 }
