6bdf94ae2dbfc234b7c53dac7774dda7b8ce87bb
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-07-17 13:58:25 +0900

    Feat: show Map with interactionModes

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -5,52 +5,13 @@
 //  Created by Groo on 7/16/24.
 //
 
+import MapKit
 import SwiftUI
 
-enum LoadingState {
-    case before
-    case loading
-    case success
-    case failed
-}
-
 struct ContentView: View {
-    @State private var loadingState = LoadingState.before
     var body: some View {
-        switch loadingState {
-        case .before:
-            BeforeView()
-        case .loading:
-            LoadingView()
-        case .success:
-            SuccessView()
-        case .failed:
-            FailedView()
-        }
-    }
-}
-
-struct BeforeView: View {
-    var body: some View {
-        Text("Before View")
-    }
-}
-
-struct LoadingView: View {
-    var body: some View {
-        Text("Loading View")
-    }
-}
-
-struct SuccessView: View {
-    var body: some View {
-        Text("Success View")
-    }
-}
-
-struct FailedView: View {
-    var body: some View {
-        Text("Failed View")
+        Map(interactionModes: [.rotate, .zoom])
+            .mapStyle(.imagery)
     }
 }
 
