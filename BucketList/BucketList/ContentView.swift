fa36c246654cb87ca3b09714427d881c42fad4f6
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-07-17 14:38:51 +0900

    Feat: set position of Berlin and Busan with onMapCameraChange

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -9,9 +9,23 @@ import MapKit
 import SwiftUI
 
 struct ContentView: View {
+    @State private var position = MapCameraPosition.region(MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 52.5200066, longitude: 13.404954), span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)))
     var body: some View {
-        Map(interactionModes: [.rotate, .zoom])
-            .mapStyle(.imagery)
+        VStack {
+            Map(position: $position)
+                .mapStyle(.hybrid)
+                .onMapCameraChange { context in
+                    print(context.region)
+                }
+            HStack {
+                Button("Berlin") {
+                    position = MapCameraPosition.region(MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 52.5200066, longitude: 13.404954), span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)))
+                }
+                Button("Busan") {                        
+                    position = MapCameraPosition.region(MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 35.2100142, longitude: 129.0688702), span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)))
+                }
+            }
+        }
     }
 }
 
