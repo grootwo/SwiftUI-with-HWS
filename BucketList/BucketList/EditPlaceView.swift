f3b2035b9aa119fcc96b3b60e46b7ba3842fd3cc
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-07-19 18:11:24 +0900

    Feat: load pages from wikipedia and show in EditPlaceView

--- BucketList/BucketList/EditPlaceView.swift
+++ BucketList/BucketList/EditPlaceView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct EditPlaceView: View {
+    enum LoadingState {
+        case loading, loaded, failed
+    }
     @Environment(\.dismiss) var dismiss
     var location: Location
     var onSave: (Location) -> Void
     @State private var name: String
+    @State private var loadingState = LoadingState.loading
+    @State private var pages = [Page]()
     var body: some View {
         NavigationStack {
             Form {
-                TextField("Place name", text: $name)
+                Section {
+                    TextField("Place name", text: $name)
+                }
+                Section("Nearby") {
+                    switch loadingState {
+                    case .loading:
+                        Text("Loading...")
+                    case .loaded:
+                        ForEach(pages, id: \.pageid) { page in
+                            Text(page.title)
+                                .font(.headline)
+                            + Text(": ") +
+                            Text("Page description here")
+                                .italic()
+                        }
+                    case .failed:
+                        Text("Please, try again.")
+                    }
+                }
             }
             .navigationTitle("Selected Place")
             .toolbar {
@@ -27,12 +50,30 @@ struct EditPlaceView: View {
                     dismiss()
                 }
             }
+            .task {
+                await fetchNearbyPlaces()
+            }
         }
     }
     init(location: Location, onSave: @escaping (Location) -> Void) {
         self.location = location
         self.onSave = onSave
         _name = State(initialValue: location.name)
+    }
+    func fetchNearbyPlaces() async {
+        let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(location.latitude)%7C\(location.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
+        guard let url = URL(string: urlString) else {
+            print("Wrong url: \(urlString)")
+            return
+        }
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let decoded = try JSONDecoder().decode(Result.self, from: data)
+            pages = decoded.query.pages.values.sorted { $0.title < $1.title }
+            loadingState = .loaded
+        } catch {
+            loadingState = .failed
+        }
     }
 }
 
