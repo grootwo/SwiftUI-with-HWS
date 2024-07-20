b28ccc156edbb5e1e8025908c7b512ad7e2cb71a
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-07-20 15:13:04 +0900

    Feat: set face id authentication to unlock places

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -12,30 +12,37 @@ struct ContentView: View {
     let startPosition = MapCameraPosition.region(MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 56, longitude: -3), span: MKCoordinateSpan(latitudeDelta: 10, longitudeDelta: 10)))
     @State private var viewModel = ViewModel()
     var body: some View {
-        MapReader { proxy in
-            Map(initialPosition: startPosition) {
-                ForEach(viewModel.locations) { location in
-                    Annotation(location.name, coordinate: location.coordinate) {
-                        Text("❣️")
-                            .font(.title)
-                            .background(.white)
-                            .clipShape(.circle)
-                            .onLongPressGesture {
-                                viewModel.selectedPlace = location
-                            }
+        if viewModel.isUnlocked {
+            MapReader { proxy in
+                Map(initialPosition: startPosition) {
+                    ForEach(viewModel.locations) { location in
+                        Annotation(location.name, coordinate: location.coordinate) {
+                            Text("❣️")
+                                .font(.title)
+                                .background(.white)
+                                .clipShape(.circle)
+                                .onLongPressGesture {
+                                    viewModel.selectedPlace = location
+                                }
+                        }
+                    }
+                }
+                .onTapGesture { position in
+                    if let coordinate = proxy.convert(position, from: .local) {
+                        viewModel.addLocation(at: coordinate)
                     }
                 }
             }
-            .onTapGesture { position in
-                if let coordinate = proxy.convert(position, from: .local) {
-                    viewModel.addLocation(at: coordinate)
+            .sheet(item: $viewModel.selectedPlace) { place in
+                EditPlaceView(location: place) {
+                    viewModel.update(location: $0)
                 }
             }
-        }
-        .sheet(item: $viewModel.selectedPlace) { place in
-            EditPlaceView(location: place) {
-                viewModel.update(location: $0)
+        } else {
+            Button("Unlock places") {
+                viewModel.authenticate()
             }
+            .buttonStyle(.borderedProminent)
         }
     }
 }
--- BucketList/ContentView-ViewModel.swift
+++ BucketList/ContentView-ViewModel.swift
@@ -8,6 +8,7 @@
 import Foundation
 import MapKit
 import CoreLocation
+import LocalAuthentication
 
 extension ContentView {
     @Observable
@@ -15,6 +16,7 @@ extension ContentView {
         private(set) var locations: [Location]
         var selectedPlace: Location?
         let savePath = URL.documentsDirectory.appending(path: "SavedPlaces")
+        var isUnlocked = false
         
         init() {
             do {
@@ -45,6 +47,23 @@ extension ContentView {
             if let index = locations.firstIndex(of: selectedPlace) {
                 locations[index] = location
                 save()
+            }
+        }
+        
+        func authenticate() {
+            let context = LAContext()
+            var error: NSError?
+            if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+                let reason = "Please authenticate yourself to unlock your places"
+                context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                    if success {
+                        self.isUnlocked = true
+                    } else {
+                        print("Unable to unlock: \(String(describing: error))")
+                    }
+                }
+            } else {
+                print("Unable to use biometrics")
             }
         }
     }
