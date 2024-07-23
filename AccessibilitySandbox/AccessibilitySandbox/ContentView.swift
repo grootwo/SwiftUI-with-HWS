c2037d8589e8d00de7829d8027404afd8a660fe6
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-07-23 17:16:09 +0900

    Feat: set accessibilityElement

--- AccessibilitySandbox/AccessibilitySandbox/ContentView.swift
+++ AccessibilitySandbox/AccessibilitySandbox/ContentView.swift
@@ -8,30 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
-    let pictures = [
-        "ales-krivec-15949",
-        "galina-n-189483",
-        "kevin-horstmann-141705",
-        "nicolas-tissot-335096"
-    ]
-    let labels = [
-        "Tulips",
-        "Frozen tree buds",
-        "Sunflowers",
-        "Fireworks"
-    ]
-    @State private var currentPicture = Int.random(in: 0..<4)
     var body: some View {
-        Button(action: {
-            currentPicture = Int.random(in: 0..<4)
-        }, label: {
-            Image(pictures[currentPicture])
-                .resizable()
-                .scaledToFit()
-        })
-        .accessibilityLabel(labels[currentPicture])
-        .accessibilityAddTraits(.isButton)
-        .accessibilityRemoveTraits(.isImage)
+        VStack {
+            Text("Hello this is")
+            Text("A TEXT")
+                .font(.title)
+        }
+        .accessibilityElement()
+        .accessibilityLabel("Hello this is a text")
     }
 }
 
