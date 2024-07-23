2969eab334c7889ec8da5705e360f4914721417e
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-07-23 14:55:34 +0900

    Feat: set func createNewLocation, and fetchNearbyPlaces

--- BucketList/BucketList/EditPlaceView+ViewModel.swift
+++ BucketList/BucketList/EditPlaceView+ViewModel.swift
@@ -23,5 +23,27 @@ extension EditPlaceView {
             self.name = location.name
         }
         
+        func createNewLocation() -> Location {
+            var newLocation = self.location
+            newLocation.id = UUID()
+            newLocation.name = self.name
+            return newLocation
+        }
+        
+        func fetchNearbyPlaces() async {
+            let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(self.location.latitude)%7C\(self.location.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
+            guard let url = URL(string: urlString) else {
+                print("Wrong url: \(urlString)")
+                return
+            }
+            do {
+                let (data, _) = try await URLSession.shared.data(from: url)
+                let decoded = try JSONDecoder().decode(Result.self, from: data)
+                self.pages = decoded.query.pages.values.sorted()
+                self.loadingState = .loaded
+            } catch {
+                self.loadingState = .failed
+            }
+        }
     }
 }
--- BucketList/BucketList/EditPlaceView.swift
+++ BucketList/BucketList/EditPlaceView.swift
@@ -37,36 +37,18 @@ struct EditPlaceView: View {
             .navigationTitle("Selected Place")
             .toolbar {
                 Button("Save") {
-                    var newLocation = viewModel.location
-                    newLocation.id = UUID()
-                    newLocation.name = viewModel.name
-                    onSave(newLocation)
+                    onSave(viewModel.createNewLocation())
                     dismiss()
                 }
             }
             .task {
-                await fetchNearbyPlaces()
+                await viewModel.fetchNearbyPlaces()
             }
         }
     }
     init(location: Location, onSave: @escaping (Location) -> Void) {
         self.onSave = onSave
         viewModel = ViewModel(location: location, name: location.name)
-    }
-    func fetchNearbyPlaces() async {
-        let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(viewModel.location.latitude)%7C\(viewModel.location.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
-        guard let url = URL(string: urlString) else {
-            print("Wrong url: \(urlString)")
-            return
-        }
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-            let decoded = try JSONDecoder().decode(Result.self, from: data)
-            viewModel.pages = decoded.query.pages.values.sorted()
-            viewModel.loadingState = .loaded
-        } catch {
-            viewModel.loadingState = .failed
-        }
     }
 }
 
