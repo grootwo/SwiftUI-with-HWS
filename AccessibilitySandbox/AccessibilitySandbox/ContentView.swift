a3ef6527d0ee06ab26b37d73bfcfcc08751d8778
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-07-23 15:51:01 +0900

    Feat: show a picture randomly with accessibility label and trait

--- AccessibilitySandbox/AccessibilitySandbox/ContentView.swift
+++ AccessibilitySandbox/AccessibilitySandbox/ContentView.swift
@@ -8,14 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    let pictures = [
+        "ales-krivec-15949",
+        "galina-n-189483",
+        "kevin-horstmann-141705",
+        "nicolas-tissot-335096"
+    ]
+    let labels = [
+        "Tulips",
+        "Frozen tree buds",
+        "Sunflowers",
+        "Fireworks"
+    ]
+    @State private var currentPicture = Int.random(in: 0..<4)
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Image(pictures[currentPicture])
+            .resizable()
+            .scaledToFit()
+            .onTapGesture {
+                currentPicture = Int.random(in: 0..<4)
+            }
+            .accessibilityLabel(labels[currentPicture])
+            .accessibilityAddTraits(.isButton)
+            .accessibilityRemoveTraits(.isImage)
     }
 }
 
