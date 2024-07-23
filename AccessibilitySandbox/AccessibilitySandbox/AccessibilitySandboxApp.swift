f3d0807f14cf05f3167acd3283a0441e9487ed23
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-07-23 15:12:38 +0900

    Feat: create new project

---
+++ AccessibilitySandbox/AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Groo on 7/23/24.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
