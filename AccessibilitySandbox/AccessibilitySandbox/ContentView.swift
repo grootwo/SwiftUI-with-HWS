b643b6211fbf31b663bec3a352aa591986657875
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-07-24 14:06:48 +0900

    Feat: set accessibilityInputLabels for voice input

--- AccessibilitySandbox/AccessibilitySandbox/ContentView.swift
+++ AccessibilitySandbox/AccessibilitySandbox/ContentView.swift
@@ -10,28 +10,10 @@ import SwiftUI
 struct ContentView: View {
     @State private var value = 0
     var body: some View {
-        VStack {
-            Text("value: \(value)")
-            Button("Increment") {
-                value += 1
-            }
-            Button("Decrement") {
-                value -= 1
-            }
+        Button("Tap to print message") {
+            print("printing message")
         }
-        .accessibilityElement()
-        .accessibilityLabel("Value")
-        .accessibilityValue(String(value))
-        .accessibilityAdjustableAction { direction in
-            switch direction {
-            case .increment:
-                value += 1
-            case .decrement:
-                value -= 1
-            default:
-                print("not handled")
-            }
-        }
+        .accessibilityInputLabels(["print", "print message", "tap to print"])
     }
 }
 
