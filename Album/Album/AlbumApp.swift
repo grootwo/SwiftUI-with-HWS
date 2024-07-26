f260d2fd507f7575f299f36af76359cea7c22a2a
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-07-26 18:51:45 +0900

    Feat: create Album project

---
+++ Album/Album/AlbumApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlbumApp.swift
+//  Album
+//
+//  Created by Groo on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct AlbumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
