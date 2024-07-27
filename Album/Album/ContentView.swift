a79c85f2ad6732d6566a36402fd1c3618435871d
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-07-27 18:00:08 +0900

    Feat: set Grid

--- Album/Album/ContentView.swift
+++ Album/Album/ContentView.swift
@@ -12,13 +12,21 @@ struct ContentView: View {
     @Environment(\.modelContext) var modelContext
     @Query(sort: \Photo.name) var photos: [Photo]
     @State private var image: Image?
+    let columns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
     var body: some View {
         NavigationStack {
-            LazyVStack {
-                VStack {
+            ScrollView {
+                LazyVGrid(columns: columns) {
                     ForEach(photos) { photo in
-                        Text(photo.name)
-                        Image(data: photo.photo)
+                        VStack {
+                            Image(data: photo.photo)!
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 150)
+                            Text(photo.name)
+                        }
                     }
                 }
             }
