d4f1fa89d759e09cd4db1486c33bea2898fb9413
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-07-29 10:17:04 +0900

    Feat: create HotProspects project

---
+++ HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Groo on 7/29/24.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
