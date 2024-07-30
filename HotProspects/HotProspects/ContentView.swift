834f484211bdaec98a1a3171c0f9adb6d7f98740
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-07-30 15:55:35 +0900

    Feat: set func fetchReadings with Result

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -8,23 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var selectedTab = 1
+    @State private var output = ""
     var body: some View {
-        TabView(selection: $selectedTab) {
-            Button("Go to tab 2") {
-                selectedTab = 2
+        Text(output)
+            .task {
+                await fetchReadings()
             }
-            .tabItem {
-                Label("One", systemImage: "1.circle")
-            }
-            .tag(1)
-            Button("Go to tab 1") {
-                selectedTab = 1
-            }
-            .tabItem {
-                Label("Two", systemImage: "2.circle")
-            }
-            .tag(2)
+    }
+    func fetchReadings() async {
+        let fetchTask = Task {
+            let url = URL(string: "https://hws.dev/readings.json")!
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let readings = try JSONDecoder().decode([Double].self, from: data)
+            return "found \(readings.count) readings"
+        }
+        
+        let result = await fetchTask.result
+        switch result {
+        case .success(let str):
+            output = str
+        case .failure(let error):
+            output = "error: \(error.localizedDescription)"
         }
     }
 }
