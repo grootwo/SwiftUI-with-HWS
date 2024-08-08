97520685e98c686eaaa23749b2773b0ba597e932
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-08-08 14:25:49 +0900

    Feat: create Flashzilla project

---
+++ Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Groo on 8/8/24.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
