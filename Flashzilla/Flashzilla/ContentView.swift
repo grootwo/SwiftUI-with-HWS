168611fe49d6b7d6f0963278450e0cec7837af06
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-08-14 14:50:47 +0900

    Feat: create Timer and print time

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -8,18 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    let timer = Timer.publish(every: 1, tolerance: 0.5, on: .main, in: .common).autoconnect()
+    @State private var counter = 0
     var body: some View {
         VStack {
-            Text("hallo")
-            Spacer().frame(height: 100)
-            Text("hola")
-        }
-        .contentShape(.rect)
-        .background(.orange)
-        .onTapGesture {
-            print("vstack")
-        }
-    }
+            Text("Hello, World!")
+                .onReceive(timer) { time in
+                    if counter == 5 {
+                        timer.upstream.connect().cancel()
+                    } else {
+                        print("The time is now \(time)")
+                        counter += 1
+                    }
+                }
+        }    }
 }
 
 #Preview {
