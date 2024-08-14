2892f23a029fe0ac971ff11a0d1a47908dbfabcd
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-08-14 16:37:44 +0900

    Feat: change blur and backgound amount depending on accessibilityReduceTransparency

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -7,24 +7,16 @@
 
 import SwiftUI
 
-func withOptionalAnimation<Result>(_ animation: Animation? = .default, _ body: () throws -> Result) rethrows -> Result {
-    if UIAccessibility.isReduceMotionEnabled {
-        return try body()
-    } else {
-        return try withAnimation(animation, body)
-    }
-}
-
 struct ContentView: View {
-    @Environment(\.accessibilityReduceMotion) var reduceMotion
-    @State private var scale = 1.0
+    @Environment(\.accessibilityReduceTransparency) var reduceTransparency
+
     var body: some View {
-        Button("Hello, World!") {
-            withOptionalAnimation {
-                scale *= 1.5
-            }
-        }
-        .scaleEffect(scale)
+        Text("Hello, World!")
+            .blur(radius: reduceTransparency ? 0 : 0.5)
+            .padding()
+            .background(reduceTransparency ? .black : .secondary)
+            .foregroundStyle(.white)
+            .clipShape(.capsule)
     }
 }
 
