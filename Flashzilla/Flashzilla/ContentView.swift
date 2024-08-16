6b405503f73a525081656b560293a7fee03d7fda
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-08-16 12:13:38 +0900

    Design: add background

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -17,9 +17,15 @@ struct ContentView: View {
     @State private var cards = Array<Card>(repeating: .example, count: 10)
     var body: some View {
         ZStack {
-            ForEach(0..<cards.count, id: \.self) { index in
-                CardView(card: cards[index])
-                    .stacked(at: index, in: cards.count)
+            Image(.background)
+                .ignoresSafeArea()
+            VStack {
+                ZStack {
+                    ForEach(0..<cards.count, id: \.self) { index in
+                        CardView(card: cards[index])
+                            .stacked(at: index, in: cards.count)
+                    }
+                }
             }
         }
     }
