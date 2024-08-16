c28c2e428de90687bb624c0efa9f1129c6ccf320
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-08-16 12:34:37 +0900

    Feat: set change depending on gesture and removing cards

--- Flashzilla/Flashzilla/CardView.swift
+++ Flashzilla/Flashzilla/CardView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct CardView: View {
     let card: Card
     @State private var isHidingAnswer = true
+    @State private var offset = CGSize.zero
+    var removal: (() -> Void)? = nil
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 25)
@@ -28,6 +30,22 @@ struct CardView: View {
             .multilineTextAlignment(.center)
         }
         .frame(width: 450, height: 250)
+        .rotationEffect(.degrees(offset.width / 5.0))
+        .offset(x: offset.width * 5)
+        .opacity(2 - Double(abs(offset.width / 50)))
+        .gesture(
+            DragGesture()
+                .onChanged { gesture in
+                    offset = gesture.translation
+                }
+                .onEnded { _ in
+                    if abs(offset.width) > 100 {
+                        removal?()
+                    } else {
+                        offset = .zero
+                    }
+                }
+        )
         .onTapGesture {
             isHidingAnswer.toggle()
         }
