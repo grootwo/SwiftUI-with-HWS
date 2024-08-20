bd7c235bb6138d45095d6e5e5ab6e726c967a538
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-08-20 09:39:05 +0900

    Feat: create LayoutAndGeometry project

---
+++ LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Groo on 8/20/24.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
