09597b6d8f125611e31f1e7fc16bc0b08b05f0e9
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-08-20 22:28:27 +0900

    Feat: set relative position fo text

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -7,38 +7,12 @@
 
 import SwiftUI
 
-extension VerticalAlignment {
-    enum MidAccountAndName: AlignmentID {
-        static func defaultValue(in context: ViewDimensions) -> CGFloat {
-            context[.top]
-        }
-    }
-
-    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
-}
-
 struct ContentView: View {
     var body: some View {
-        HStack(alignment: .midAccountAndName) {
-            VStack {
-                Image(.example)
-                    .resizable()
-                    .frame(width: 64, height: 64)
-                Text("@grootwo")
-                    .alignmentGuide(.midAccountAndName) { d in
-                        d[VerticalAlignment.center]
-                    }
-            }
-
-            VStack {
-                Text("Full name:")
-                Text("Groo Two")
-                    .alignmentGuide(.midAccountAndName) { d in
-                        d[VerticalAlignment.center]
-                    }
-                    .font(.largeTitle)
-            }
-        }
+        Text("Hallo und Hallo")
+            .font(.title)
+            .offset(x: 250, y: 250)
+            .background(.yellow)
     }
 }
 
