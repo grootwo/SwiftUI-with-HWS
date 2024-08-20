50d23e5b063236c169384b7513a75c1eec4995fb
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-08-20 12:06:52 +0900

    Feat: show an image with frame

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -9,13 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Image(.example)
+            .frame(width: 100, height: 100)
     }
 }
 
