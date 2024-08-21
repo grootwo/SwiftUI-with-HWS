4c8afd97be39ce4c721d748da89f7d1ff3a13630
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-08-21 10:13:44 +0900

    Feat: set HStack with GeometryReader

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -9,10 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hallo und Hallo")
-            .font(.title)
-            .offset(x: 250, y: 250)
-            .background(.yellow)
+        HStack {
+            Text("IMPORTANT")
+                .frame(width: 200)
+                .background(.blue)
+            
+            GeometryReader { proxy in
+                Image(.example)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: proxy.size.width * 0.8, height: proxy.size.height)
+            }
+        }
     }
 }
 
