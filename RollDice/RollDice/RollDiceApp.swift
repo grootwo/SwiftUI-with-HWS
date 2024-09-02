d60eb5c1e196c246c6d2f026ea3533a191964078
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-09-02 17:11:16 +0900

    Feat: create RollDice project

---
+++ RollDice/RollDice/RollDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollDiceApp.swift
+//  RollDice
+//
+//  Created by Groo on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct RollDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
