84781d684a6b8ce5bcdba5394af57d15bb8ad139
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-09-02 17:38:05 +0900

    Feat: create RollDiceView and set rolling dice

---
+++ RollDice/RollDice/RollDiceView.swift
@@ -0,0 +1,23 @@
+//
+//  RollDiceView.swift
+//  RollDice
+//
+//  Created by Groo on 9/2/24.
+//
+
+import SwiftUI
+
+struct RollDiceView: View {
+    @State private var newNumber: Int? = nil
+    var body: some View {
+        Text(newNumber == nil ? "\(newNumber)" : "?")
+        Button("Roll", action: rollDice)
+    }
+    func rollDice() {
+        newNumber = Int.random(in: 1...6)
+    }
+}
+
+#Preview {
+    RollDiceView()
+}
