82c0b2e77d0aa4e315f288e27a02630c127443c7
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-09-03 15:16:01 +0900

    Feat: create SnowSeeker project

---
+++ SnowSeeker/SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Groo on 9/3/24.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
