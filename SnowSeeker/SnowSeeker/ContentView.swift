707c6eba0fc6c6834b33540b4d0edb721e4577ae
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-09-03 17:34:10 +0900

    Feat: share @Observabler with Environment and Bindable

--- SnowSeeker/SnowSeeker/ContentView.swift
+++ SnowSeeker/SnowSeeker/ContentView.swift
@@ -7,24 +7,27 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    @State private var searchText = ""
-    let animals = ["cat", "dog", "elephant", "owl", "tiger", "koala"]
-    var filteredAnimals: [String] {
-        if searchText.isEmpty {
-            return animals
-        } else {
-            return animals.filter { $0.localizedStandardContains(searchText) }
-        }
-    }
-    
+@Observable
+class Player {
+    var name = "Anonymous"
+    var highScore = 0
+}
+
+struct ScoreView: View {
+    @Environment(Player.self) var player
     var body: some View {
-        NavigationStack {
-            List(filteredAnimals, id: \.self) { animal in
-                Text(animal)
-            }
-            .searchable(text: $searchText, prompt: "Look for something")
+        @Bindable var player = player
+        Stepper("Your high score: \(player.highScore)", value: $player.highScore)
+    }
+}
+
+struct ContentView: View {
+    @State private var player = Player()
+    var body: some View {
+        VStack {
+            ScoreView()
         }
+        .environment(player)
     }
 }
 
