896db30bb866c9e88c262a3458223380663dc9f1
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-09-04 14:32:41 +0900

    Feat: set ContentView with NavigationSplitView

--- SnowSeeker/SnowSeeker/ContentView.swift
+++ SnowSeeker/SnowSeeker/ContentView.swift
@@ -8,12 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    let resorts: [Resort] = Bundle.main.decode(file: "resorts.json")
     var body: some View {
-        ViewThatFits {
-            Rectangle()
-                .frame(width: 500, height: 200)
-            Circle()
-                .frame(width: 200, height: 200)
+        NavigationSplitView {
+            List(resorts) { resort in
+                NavigationLink(value: resort) {
+                    HStack {
+                        Image(resort.country)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 40, height: 25)
+                            .clipShape(.rect(cornerRadius: 5))
+                            .overlay(RoundedRectangle(cornerRadius: 5)
+                                .stroke(.black, lineWidth: 1)
+                            )
+                        VStack(alignment: .leading) {
+                            Text(resort.name)
+                                .font(.headline)
+                            Text("\(resort.runs) runs")
+                                .foregroundStyle(.secondary)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Resorts")
+        } detail: {
+            Text("This is Detail")
         }
     }
 }
