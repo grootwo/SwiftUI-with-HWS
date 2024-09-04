1d705f9f6aa6ded57ba06d4314a07a97c3d9a161
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-09-04 14:13:33 +0900

    Feat: create Resort from Bundle decoded

---
+++ SnowSeeker/SnowSeeker/Resort.swift
@@ -0,0 +1,25 @@
+//
+//  Resort.swift
+//  SnowSeeker
+//
+//  Created by Groo on 9/4/24.
+//
+
+import Foundation
+
+struct Resort: Codable, Hashable, Identifiable {
+    let id: String
+    let name: String
+    let country: String
+    let description: String
+    let imageCredit: String
+    let price: Int
+    let size: Int
+    let snowDepth: Int
+    let elevation: Int
+    let runs: Int
+    let facilities: [String]
+    
+    static let resorts: [Resort] = Bundle.main.decode(file: "resorts.json")
+    static let example = resorts[0]
+}
