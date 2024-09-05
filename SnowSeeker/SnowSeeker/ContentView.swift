ddfb13077e9f9b546bfcd257391e07c9f3c12382
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-09-05 14:21:49 +0900

    Feat: set searchable for resorts

--- SnowSeeker/SnowSeeker/ContentView.swift
+++ SnowSeeker/SnowSeeker/ContentView.swift
@@ -8,10 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var searchText = ""
     let resorts: [Resort] = Bundle.main.decode(file: "resorts.json")
+    var filteredResorts: [Resort] {
+        if searchText.isEmpty {
+            return resorts
+        } else {
+            return resorts.filter { $0.name.localizedStandardContains(searchText) }
+        }
+    }
     var body: some View {
         NavigationSplitView {
-            List(resorts) { resort in
+            List(filteredResorts) { resort in
                 NavigationLink(value: resort) {
                     HStack {
                         Image(resort.country)
@@ -35,6 +43,7 @@ struct ContentView: View {
             .navigationDestination(for: Resort.self) { resort in
                 ResortView(resort: resort)
             }
+            .searchable(text: $searchText, prompt: "Search for a resort")
         } detail: {
             WelcomeView()
         }
